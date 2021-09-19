3561330de9c81156e60ec0a478852020de8a8d66
cdsanto <chris.salazar.ipn@gmail.com>
2021-09-19 17:21:28 -0500

    change all files

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,8 @@ RUN yarn
 # Copying the rest of the code to the working directory
 COPY . .
 
-# Make port 3000 available to the world outside this container
-EXPOSE 3000
+# Make port 80 available to the world outside this container
+EXPOSE 80
 
 # Run index.js when the container launches
 CMD ["node", "index.js"]
--- variables.tf
+++ variables.tf
@@ -1,10 +1,45 @@
-variable "pub_subnet" {
-  type    = list
-  #default = ["subnet-0702a73062452dfea","subnet-07bc53c36d143df33","subnet-08057e4dbfc49cb9e"]
-  default = ["subnet-0612888dbb865e9af","subnet-008cc2d8a1d850eba","subnet-006161aa498c1f346"]
+# variables.tf
+
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "us-east-2"
 }
-variable "vpcid" {
-  type    = string
-  #default = "vpc-057a96227c4597b48"
-  default = "vpc-0a51f227e51f6bec8"
+
+variable "ecs_task_execution_role_name" {
+  description = "ECS task execution role name"
+  default = "myEcsTaskExecutionRole"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "nginx:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 80
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 1
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
 }
